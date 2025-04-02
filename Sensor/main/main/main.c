cb3321c137d539bb86cf373f3381e55884bbd506
andno773 <andno773@student.liu.se>
2025-04-02 16:14:36 +0200

    Organiserat om filer.

--- Sensor/main/main/main.c
+++ Sensor/main/main/main.c
@@ -1,45 +1,36 @@
 /*
- * Avstnd_test.c
+ * main.c
  *
- * Created: 2025-04-02 08:19:26
+ * Created: 2025-04-02 15:58:02
  * Author : andno773
  */ 
 
 #include <avr/io.h>
 #include <avr/interrupt.h>
 #include <math.h>
-#include "Math.c"
 #include <stdio.h>
+#include "init.c"
+#include "convert.c"
 
 int indata = 0;
 int dist = 0;
-int test = 0;
 
-
-int main(void)
+int main()
 {
-    DDRB = 0xff;
-	DDRD = 0x00;
-	PORTD = 0x00;
-	
-	ADMUX = (1<<REFS0)|(1<<ADLAR)|(1<<MUX2)|(1<<MUX1);
-	ADCSRA = (1<<ADEN)|(0<<ADSC)|(0<<ADATE)|(0<<ADIF)|(1<<ADIE)|(1<<ADPS2)|(1<<ADPS1)|(1<<ADPS0);
-	
-	EICRA |=(1<<ISC01)|(1<<ISC00);
-	EIMSK |= (1<<INT0);
+    init_IR();
+	init_gyro();
+	init_reflex();
+	init_interrupt();
 	
 	sei();
 	
     while (1) 
     {
-		//sei();
     }
 }
 
-
 ISR(INT0_vect)
 {
-	//cli();
 	ADCSRA |= (1 << ADSC);
 }
 
@@ -50,6 +41,5 @@ ISR(ADC_vect)
 	PORTB = indata_bin;
 	indata = convert_uint8_t(indata_bin);
 	dist = volt_to_dist(indata);
-	test = 4;
 	sei();
 }
