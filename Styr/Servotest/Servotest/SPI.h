f30e1862a1dfe4cbcb518ab5091f8eac0daef00d
linfu930 <linfu930@student.liu.se>
2025-04-16 14:59:22 +0200

    Tejpföljning påbörjad

--- Styr/Servotest/Servotest/SPI.h
+++ Styr/Servotest/Servotest/SPI.h
@@ -1,9 +1,14 @@
 
 volatile uint8_t current_action = 0;
 volatile uint8_t last_action = 0;
+volatile float reglerstyr = 0;
 
 ISR(SPI_STC_vect) {
 	// SPI interrupt: när data mottas, läs från SDPR (SPI Data Register)
 	current_action = SPDR;
+	if(current_action == 0x30)
+	{
+		reglerstyr = SPDR;
+	}
 
 }
