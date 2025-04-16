1ad71441944ec662f7e2f1b149e404c7aca72ff4
ebblu474 <ebblu474@student.liu.se>
2025-04-16 10:18:55 +0200

    fixat så vi kan skicka data mellan pc och raspberry, grunden

--- PC/Bluetooth.py
+++ PC/Bluetooth.py
@@ -1,12 +1,12 @@
 import socket
 
-
 def bluetoothinit():
     global s
     s = socket.socket(socket.AF_BLUETOOTH, socket.SOCK_STREAM, socket.BTPROTO_RFCOMM)
 
     try:
         s.connect(('B8:27:EB:E9:12:27', 4))
+        print("Ansluten till raspberry pi")
 
     except:
         print("koppling till raspberry misslyckades")
@@ -17,14 +17,14 @@ def sendbyte(byte):
     s.send(byte.to_bytes(1, 'big'))
     return
 
-#while 1:
-#    data = input()
-#    if(data == "w"):
-#        s.send((1).to_bytes(1, 'big'))
-#    elif(data == "a"):
-#        s.send((2).to_bytes(1, 'big'))
-#    elif(data == "t"):
-#        s.send((3).to_bytes(1, 'big'))
-#     elif(data == "stop"):
-#        break
+
+while 1:
+    if(input("wat") == "w"):
+        size = 1024
+        bluetoothinit()
+        sendbyte(0xFF)
+        data = s.recv(size)
+        print(data)
+
+
 
