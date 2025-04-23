430a6c41169e0b75b4997f29d9409355e12fdfea
linfu930 <linfu930@student.liu.se>
2025-04-23 17:30:29 +0200

    Småpill med reglering

--- Styr/Servotest/Servotest/SPI.h
+++ Styr/Servotest/Servotest/SPI.h
@@ -1,32 +1,4 @@
-/*
-volatile uint8_t current_action = 0;
-volatile uint8_t last_action = 0;
-volatile uint8_t second_last_action = 0;
-volatile uint8_t reglerstyr_high = 0;
-volatile uint8_t reglerstyr_low = 0;
-volatile float reglerstyr = 0;
 
-ISR(SPI_STC_vect) {
-	// SPI interrupt: när data mottas, läs från SDPR (SPI Data Register)
-	if(last_action == 0x30)
-	{
-		reglerstyr_high = SPDR;
-		
-		second_last_action = last_action;
-		
-	}
-	else if(second_last_action == 0x30)
-	{
-		reglerstyr_low = SPDR;
-		reglerstyr = (float)(reglerstyr_high << 8) | reglerstyr_low
-	}
-	else
-	{
-		current_action = SPDR;
-		
-	}
-}
-*/
 
 volatile enum {
 	SPI_STATE_WAITING,
@@ -44,20 +16,20 @@ volatile uint8_t last_action = 0;
 
 ISR(SPI_STC_vect) {
 	uint8_t received = SPDR;
-
 	switch (spi_state) {
 		case SPI_STATE_WAITING:
 		if (received == 0x30) {
 			spi_state = SPI_STATE_RECEIVE_TECKEN;
+			current_action = received;
+		} else {
+			current_action = received;
+			
 		}
-		else
-		{
-			current_action = received;	
-		}
+
 		break;
 		
 		case SPI_STATE_RECEIVE_TECKEN:
-		reglertecken = SPDR;
+		reglertecken = received;
 		spi_state = SPI_STATE_RECEIVE_HIGH;
 		break;
 		
@@ -70,8 +42,10 @@ ISR(SPI_STC_vect) {
 
 		case SPI_STATE_RECEIVE_LOW:
 		reglerstyr_low = received;
+		cli();
 		reglerstyr = (float)((reglerstyr_high << 8) | reglerstyr_low);
 		reglerstyr /= 100;
+		sei();
 		if(reglertecken == 1)
 		{
 			reglerstyr *= -1;
